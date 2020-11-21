23b5b153abba8afda2956e93489bf0e9a29e7cc8
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-11-21 14:18:31 +0100

    No new line

--- R/musicobservatory.R
+++ R/musicobservatory.R
@@ -5,6 +5,10 @@
 #' @section refresh functions:
 #' The refresh functions check if new data is available from a public
 #' data resource or API, and if necessary, they refresh the data.
+#' 
+#' @section Eurostat functions:
+#' Eurostat function process data and metadata from raw data acquired from
+#' Eurostat.
 #'
 #' @docType package
 #' @name musicobservatory
